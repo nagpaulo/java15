054995f48d0e05b49a6d084edea1706a437ea270
paulo.roberto <paulo.roberto@seduc.ce.gov.br>
2020-10-09 16:53:43 -0300

    Configurando swagger

--- src/main/java/br/com/projeto/estudos/config/SwaggerConfig.java
+++ src/main/java/br/com/projeto/estudos/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package br.com.projeto.estudos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 	@Bean
 	public Docket greetingApi() {
@@ -37,4 +40,9 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
+	
+	@Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }    
 }
--- src/main/java/br/com/projeto/estudos/config/WebSecurityConfig.java
+++ src/main/java/br/com/projeto/estudos/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,13 +21,8 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableResourceServer
-@EnableAuthorizationServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	
-	
 	
 	@Bean
 	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
@@ -46,8 +42,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable().exceptionHandling().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests().antMatchers("/api/**",
-						"/actuator/**", "/auth/**", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/v2/*")
+						"/actuator/**", "/auth/**", "/swagger-resources/**", "/swagger-ui.html", "/swagger-ui/**", "/webjars/**", "/v2/*", "/v3/*")
 				.permitAll().anyRequest().authenticated();
 		httpSecurity.headers().cacheControl();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/swagger-ui/**", "/v3/api-docs/**");
 	}
 }
